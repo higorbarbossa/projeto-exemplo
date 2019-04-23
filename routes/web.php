c0ccd1ebb3742aaa4b5a13bd7acc2f146f309125
Higor Barbosa <higor.s009@gmail.com>
2019-04-23 00:31:21 -0300

    Configurações do projeto
    Linguagem
    timezone
    rotas

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('clients', 'ClientController');
+
+Route::get('/vue/{vue_capture?}', function () {
+    return view('vue.index');
+   })->where('vue_capture', '[\/\w\.-]*');
